f520fd19598d57fdf555840043572b17da93c658
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-05-19 23:10:00 +0530

    ADDED:Welcome message

---
+++ src/main/java/com/bridgelabz/LineComparator.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class LineComparator {
+    public static void main(String args[])
+    {
+        System.out.println("Welcome to Line Comparison Computation Program");
+    }
+}
