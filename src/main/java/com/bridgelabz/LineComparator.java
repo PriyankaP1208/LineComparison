4d5fa347210624e8be6f9a37e04fe43e129072b7
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-05-20 13:01:59 +0530

    ADDED:Functionality to check equality of two lines

--- src/main/java/com/bridgelabz/LineComparator.java
+++ src/main/java/com/bridgelabz/LineComparator.java
@@ -1,5 +1,6 @@
 package com.bridgelabz;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class LineComparator {
@@ -7,21 +8,31 @@ public class LineComparator {
     {
         System.out.println("Welcome to Line Comparison Computation Program");
         int x1,x2,y1,y2;
-        int length;
+        int a1,a2,b1,b2;
+        int length1;
+        int length2;
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter x1 point");
+        System.out.println("enter x1 point and y1");
         x1=sc.nextInt();
-
-        System.out.println("enter y1 point");
         y1=sc.nextInt();
 
-        System.out.println("enter x2point");
+        System.out.println("enter x2point y2");
         x2=sc.nextInt();
-
-        System.out.println("enter y2point");
         y2=sc.nextInt();
 
-        length= (int) Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-        System.out.println("Length of line:"+length);
+        System.out.println("enter a1 point a2");
+        a1=sc.nextInt();
+        a2=sc.nextInt();
+
+        System.out.println("enter b1 point b2");
+        b1=sc.nextInt();
+        b2=sc.nextInt();
+
+        length1= (int) Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        length2= (int) Math.sqrt((a2-a1)*(a2-a1) + (b2-b1)*(b2-b1));
+        System.out.println("Length of line:"+length1);
+        System.out.println("Length of line:"+length2);
+        System.out.println("Comparing" + length1 + " and " + length2 + " : " + Objects.equals(length1, length2));
+
     }
 }
